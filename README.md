ed9026610a0f1daa0055f5f887de98064c05c751
Trevor Brooks <trevorism@gmail.com>
2026-09-02 11:06:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# certs
+![Build](https://github.com/trevorism/certs/actions/workflows/deploy.yml/badge.svg)
+![GitHub last commit](https://img.shields.io/github/last-commit/trevorism/certs)
+![GitHub language count](https://img.shields.io/github/languages/count/trevorism/certs)
+![GitHub top language](https://img.shields.io/github/languages/top/trevorism/certs)
+
+# How to build
+`gradle clean build`
---
+++ src/app/vitest.config.js
@@ -0,0 +1,17 @@
+import { fileURLToPath } from 'node:url'
+import { mergeConfig } from 'vite'
+import { configDefaults, defineConfig } from 'vitest/config'
+import TrevorismTestResultReporter from '@trevorism/vitest-test-result-events'
+import viteConfig from './vite.config'
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'e2e/*'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      reporters: ['default', new TrevorismTestResultReporter('certs')]
+    }
+  })
+)
